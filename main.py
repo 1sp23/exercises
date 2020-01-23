3386ae123537d5eade2770fcc2d65b9bc70ee9d9
1sp23 <sofya.pavlusha@gmail.com>
2020-01-23 21:19:44 +0700

    04

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 print('Hello, Python!')
+
 print('Привет, Python!\n'
       'Hello, Python!\n'
       'Bonjour Python!\n'
       'Hej, Python!\n'
       'Hola, Python!')
+
+print('Привет')
+print('Python')
+
+print('(\\___/)')
+print('(=\'.\'=)')
+print('(\")_(\")')
